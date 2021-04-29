708e1bdcb1befc13a7cf374a03c292e5d24f0a75
Lexo <florentdebuchy@orange.fr>
2021-04-29 12:46:12 +0000

    wordpress fix

--- scriptWordPress.sh
+++ scriptWordPress.sh
@@ -16,7 +16,23 @@ if [ -z $SQL_PASSWORD ]; then
  read -p "Enter your sql password : " SQL_PASSWORD
 fi
 
+print(){
 
+ echo "\n /**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key>
+ * You can change these at any point in time to invalidate all existing cookies>
+ *
+ * @since 2.6.0
+ */
+
+ $WORDPRESS_KEYS
+
+ /**#@-*/" >>  wp-config.copy.php
+
+}
 
 DIR="/var/www/${DOMAIN_NAME}"
 mkdir $DIR
@@ -36,33 +52,20 @@ if [ -d $DIR ]; then
  cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
  mkdir /tmp/wordpress/wp-content/upgrade
  sudo cp -a /tmp/wordpress/. $DIR
- sed $DIR
- WORDPRESS_KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
- cp "./wp-config.template.php ./wp-config.copy.php"
- sed -i 's/word_press_keys/`$WORDPRESS_KEYS`/g' 'wp-config.php'
- mv wp-config.copy.php  /var/www/$DOMAIN_NAME/wp-config.php
-else
- echo "Error file didn't exist"
-fi
-
 
  WORDPRESS_KEYS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+ cd /projetGit
  cp wp-config.template.php wp-config.copy.php
 
  sed -i -e s/database_name_here/${DB_NAME}/g 'wp-config.copy.php'
  sed -i -e s/username_here/${SQL_USER}/g 'wp-config.copy.php'
  sed -i -e s/password_here/${SQL_PASSWORD}/g 'wp-config.copy.php'
 
- echo "\n /**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+ print
 
- $WORDPRESS_KEYS
-
- /**#@-*/" >>  wp-config.copy.php
+ mv /var/www/$DOMAIN_NAME/wp-config.php /var/www/$DOMAIN_NAME/wp-oldconfig.php
+ mv wp-config.copy.php /var/www/$DOMAIN_NAME/wp-config.php
+ echo https://$WORDPRESS_KEYS
+else
+ echo "Error file didn't exist"
+fi
