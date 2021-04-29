22829ddfb6de5a9158ea462ec988e5f30db186a6
Lexo <florentdebuchy@orange.fr>
2021-04-29 21:26:59 +0000

    ssh

--- scriptBackup.sh
+++ scriptBackup.sh
@@ -1,3 +1,10 @@
+#!bin/bash
+
 tar -cz /var/www | (cd /var/www ; tar -xvz)
 cd /var/www
-cp -r var /var/backup
+cp -r var /var/backups
+rm -r var
+cd /var/backups
+mv var date-codingvm
+
+scp -r /var/backups/date-codingvm back@192.168.1.32:/var/backups
