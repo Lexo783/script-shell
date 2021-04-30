a9ccddeea1825cbc2c41fb3070a6f2f0f3abf026
Francesco-Hart <ben.francesco23@gmail.com>
2021-04-30 16:41:10 +0200

    change mysql file

--- scriptSQL.sh
+++ scriptSQL.sh
@@ -1,6 +1,7 @@
 #!bin/bash
-
-echo "CREATE DATABASE IF NOT EXISTS ${DOMAIN_NAME};" | mysql -u root -p
-echo "create user ${SQL_USER}@'%' identified with mysql_native_password by '${SQL_PASSWORD}';" | mysql -u root -p
-echo "grant all on ${DB_NAME}.* to ${SQL_USER}@'%';" | mysql -u root -p
-echo "flush privileges;" | mysql -u root -p
+mysql -u root -p
+echo "CREATE DATABASE IF NOT EXISTS ${DOMAIN_NAME};" 
+echo "create user ${SQL_USER}@'%' identified with mysql_native_password by '${SQL_PASSWORD}';" 
+echo "grant all on ${DB_NAME}.* to ${SQL_USER}@'%';" 
+echo "flush privileges;" 
+echo "EXIT;" 
