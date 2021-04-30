535ba24e3affab15af7d096e055f707abafa50ce
Francesco-Hart <ben.francesco23@gmail.com>
2021-04-30 16:44:43 +0200

    db

--- scriptSQL.sh
+++ scriptSQL.sh
@@ -1,7 +1,6 @@
 #!bin/bash
-mysql -u root -p
-echo "CREATE DATABASE IF NOT EXISTS ${DOMAIN_NAME};" 
-echo "create user ${SQL_USER}@'%' identified with mysql_native_password by '${SQL_PASSWORD}';" 
-echo "grant all on ${DB_NAME}.* to ${SQL_USER}@'%';" 
-echo "flush privileges;" 
-echo "EXIT;" 
+
+echo "CREATE DATABASE IF NOT EXISTS ${DOMAIN_NAME};" | mysql -u root -p
+echo "create user ${SQL_USER}@'%' identified with mysql_native_password by '${SQL_PASSWORD}';" | mysql -u root -p
+echo "grant all on ${DB_NAME}.* to ${SQL_USER}@'%';" | mysql -u root -p
+echo "flush privileges;" | mysql -u root -p
