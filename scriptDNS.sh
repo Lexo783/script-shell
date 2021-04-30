07b19099c4932512b44747862da8f57a62a4ff2f
Francesco-Hart <ben.francesco23@gmail.com>
2021-04-30 12:09:44 +0200

    change làogs dir

--- scriptDNS.sh
+++ scriptDNS.sh
@@ -9,8 +9,8 @@ printVirtualHost(){
      ServerAlias www.$DOMAIN_NAME
      ServerAdmin webmaster@localhost
      DocumentRoot /var/www/$DOMAIN_NAME
-     ErrorLog ${APACHE_LOG_DIR}/error.log
-     CustomLog ${APACHE_LOG_DIR}/access.log combined
+     ErrorLog /var/log/apache2/access.$DOMAIN_NAME.log
+     CustomLog /var/log/apache2/access.$DOMAIN_NAME.log
  </VirtualHost>
  <Directory /var/www/wordpress/>
     AllowOverride All
