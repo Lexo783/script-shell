a8bcd41565d73bccb6697b7ade6bbe8aef150305
Lexo <florentdebuchy@orange.fr>
2021-05-01 12:01:00 +0000

    final fix

--- scriptBackup.sh
+++ scriptBackup.sh
@@ -3,18 +3,36 @@
 DOMAIN_NAME=unset
 SQL_USER=unset
 SQL_NAME=unset
+IPVM=unset
+USERVM=unset
 
-while getopts 'u:n:' o
+while getopts 'u:n:i:e:' o
 do
     # si il trouve l'option il attribu #
     case $o in
      u) SQL_USER=$OPTARG;;
      n) SQL_NAME=$OPTARG;;
+     i) IPVM=$OPTARG;;
+     e) USERVM=$OPTARG;;
     esac
 done
 
+usage()
+{
+  echo "Usage: initialisation
+    [ -u ] [ -u MYSQL_USER]
+    [ -n ] [ -n DATABASE_NAME ]
+    [ -i ] [ -i YOUR_IP_VM ]
+    [ -e ] [ -e USER_VM ]"
+  exit 2
+}
 
-
+if [ -z $IPVM ]; then
+ usage
+fi
+if [ -z $USERVM ]; then
+ usage
+fi
 tar -cz /var/www | (cd /var/www ; tar -xvz)
 cd /var/www
 cp -r var /var/backups
@@ -24,4 +42,4 @@ mv var date-codingvm
 cd /date-codingvm
 mysqldump -u $SQL_USER -p $SQL_NAME > $SQL_NAME-backup.sql
 
-scp -r /var/backups/date-codingvm back@192.168.1.32:~/
+scp -r /var/backups/date-codingvm $USERVM@$IPVM:~/
